36b023e60b5e3c199ea9186a2851f2f58c86e3e2
Viraj37935 <virajdimble6@gmail.com>
2026-03-14 11:32:09 +0530

    test Sumbmit

---
+++ Java_Test/src/Test/JavaTest0005.java
@@ -0,0 +1,18 @@
+package Test;
+
+public class JavaTest0005 {
+
+	public static void main(String[] args) {
+		
+		int number = 1;
+		
+		for(int i = 1; i<=5;i++) {
+			for(int j =1; j <=i;j++) {
+				System.out.print(number + " ");
+				number++;
+		}
+			System.out.println();
+
+	}
+	}
+}

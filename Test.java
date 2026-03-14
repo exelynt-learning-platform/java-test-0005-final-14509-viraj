e6363a030d07850d5e0766cdb6c6eba0a3cec6e4
Viraj Dimble <129368421+Viraj37935@users.noreply.github.com>
2026-03-14 11:11:28 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,20 @@
+package javatest0005;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 
+		int number = 1 ;
+		
+		for(int i = 1; i<=5;i++){
+			for(int j =1; j <= i; j++) {
+			
+				System.out.print(number + " ");
+				number++;
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
